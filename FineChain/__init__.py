7446b08452dfb3680b5e04507e07a3bcae111b83
Jake Rowland <jakerowland@verizon.net>
2018-02-24 20:54:04 -0600

    Testing endpoints

--- FineChain/__init__.py
+++ FineChain/__init__.py
@@ -3,9 +3,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/user', methods=['POST', 'PUT'])
 def hello_world():
 	return 'Hello World!'
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def getUser(user_id):
+    return f'GET-Gets the user with id {user_id}'
+
 
 if __name__ == '__main__':
 	app.run()
