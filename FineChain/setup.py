16870a791bda113453830f0d9613f3586de1ddf8
Jake Rowland <jakerowland@verizon.net>
2018-02-27 17:11:49 -0600

    Acutally commiting setup.py

---
+++ FineChain/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ServerUtils',
+      version='1.0',
+      description='Basic utilities for the FineChain server',
+      author='Jake Rowland',
+      author_email='jwrowland@smu.edu',
+      url='https://github.com/lazysp333/FineChain/',
+      packages=['server'],
+     )
