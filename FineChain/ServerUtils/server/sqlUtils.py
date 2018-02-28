6744af421ed4bcd4d44b18d47e44a83c0cd23228
Jake Rowland <jakerowland@verizon.net>
2018-02-27 21:27:09 -0600

    Working on sql syntax

--- FineChain/ServerUtils/server/sqlUtils.py
+++ FineChain/ServerUtils/server/sqlUtils.py
@@ -8,7 +8,7 @@ connection = connector.connect(
 
 get_user_with_id = ("SELECT id, name, email, company_id, username, created_at, updated_at, deleted_at"
                     "FROM users"
-                    "WHERE users.id = %(id)s")
+                    "WHERE id = %(id)s")
 insert_user =       ("INSERT INTO users "
                      "(name, email, username, password, salt) "
                      "VALUES (%(name)s, %(email)s, %(username)s, %(password)s, %(salt)s)")
