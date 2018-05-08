6c44deba61a9712c8bce762de8c32ef0c1edb06d
jake.rowland <jakerowland@verizon.net>
2018-05-08 18:30:36 -0500

    Added comments to my code

--- FineChain/FineChainAPI/Session.py
+++ FineChain/FineChainAPI/Session.py
@@ -1,7 +1,9 @@
+# Session class to help session persistance
 class Session():
     session_token = ''
     refresh_token = ''
 
+    # Created with a session and refresh token
     def __init__(self, session=None, refresh=None):
         self.session_token = session
         self.refresh_token = refresh
